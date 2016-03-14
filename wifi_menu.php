7348edb187939a21430c6cbb72e66e7f40cb3522
TrystanLea <glyn.hudson@openenergymonitor.org>
2016-03-14 14:35:27 +0000

    merge 9.0 into master and changed menu back

--- wifi_menu.php
+++ wifi_menu.php
@@ -1,3 +1,3 @@
 <?php
 
-    $menu_dropdown_config[] = array('name'=> dgettext($domain, "Wifi"), 'icon'=>'icon-signal', 'path'=>"wifi" , 'session'=>"write", 'order' => 35, 'divider'=>true );
+    $menu_right[] = array('name'=> dgettext($domain, "Wifi"), 'path'=>"wifi" , 'session'=>"write");
