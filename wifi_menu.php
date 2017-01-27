5d4dfe2e6d61cc569a6da41746f6e5ae580732a8
Glyn Hudson <glyn.hudson@gmail.com>
2017-01-27 16:35:26 +0100

    menu, typo and button tweaks

--- wifi_menu.php
+++ wifi_menu.php
@@ -1,3 +1,3 @@
 <?php
 
-    $menu_dropdown_config[] = array('name'=> dgettext($domain, "Wifi"), 'icon'=>'icon-signal', 'path'=>"wifi" , 'session'=>"write", 'order' => 35, 'divider'=>true );
+    $menu_dropdown_config[] = array('name'=> dgettext($domain, "WiFi"), 'icon'=>'icon-signal', 'path'=>"wifi" , 'session'=>"write", 'order' => 1, 'divider'=>false );
